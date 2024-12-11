570c6949320ae34a5ae78571a9857cd440194401
brasilisnet <83701053+brasilisnet@users.noreply.github.com>
2024-12-11 12:12:20 +0900

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,46 @@
+/*
+ * Url preview script 
+ * powered by jQuery (http://www.jquery.com)
+ * 
+ * written by Alen Grakalic (http://cssglobe.com)
+ * 
+ * for more info visit http://cssglobe.com/post/1695/easiest-tooltip-and-image-preview-using-jquery
+ *
+ */
+ 
+this.screenshotPreview = function(){	
+	/* CONFIG */
+		
+		xOffset = 10;
+		yOffset = 30;
+		
+		// these 2 variable determine popup's distance from the cursor
+		// you might want to adjust to get the right result
+		
+	/* END CONFIG */
+	$("a.screenshot1").hover(function(e){
+		this.t = this.title;
+		this.title = "";	
+		var c = (this.t != "") ? "<br/>" + this.t : "";
+		$("body").append("<p id='screenshot1'><img src='"+ this.rel +"' alt='url preview' />"+ c +"</p>");								 
+		$("#screenshot1")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px")
+			.fadeIn("fast");						
+    },
+	function(){
+		this.title = this.t;	
+		$("#screenshot1").remove();
+    });	
+	$("a.screenshot1").mousemove(function(e){
+		$("#screenshot1")
+			.css("top",(e.pageY - xOffset) + "px")
+			.css("left",(e.pageX + yOffset) + "px");
+	});			
+};
+
+
+// starting the script on page load
+$(document).ready(function(){
+	screenshotPreview();
+});
